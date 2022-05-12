f78da626963c758b806d6f274a12457e47ba2b42
Sireis <JensDieterUlrich@hotmail.de>
2022-05-12 20:37:31 +0200

    updated application to use webbase and webflash

--- main/main.c
+++ main/main.c
@@ -1,5 +1,50 @@
 #include <stdio.h>
+#include "nvs_flash.h"
+#include "webBase.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+
+#include "cmake_build_info.h"
+#include "webBase.h"
+#include "webFlash.h"
+
+#include "mbedtls/error.h"
+
+static void initSystemComponents();
+static void initUserComponents();
+static void configureComponents();
+static void startComponents();
 
 void app_main(void)
 {    
+    printf("Based on commit hash: %s\n", GIT_COMMIT_HASH);
+    printf("Build/Flash time:     %s\n", BUILD_TIME);
+
+    initSystemComponents();
+    initUserComponents();
+    configureComponents();
+    startComponents();
+}
+
+void initSystemComponents()
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+}
+
+void initUserComponents()
+{
+    webBase_init();
+    webFlash_init();
+}
+
+void configureComponents()
+{
+}
+
+void startComponents()
+{
+    webBase_start();
+    webFlash_start();
 }
